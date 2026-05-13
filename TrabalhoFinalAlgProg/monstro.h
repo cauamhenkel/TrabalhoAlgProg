05b094bcd98315bd5ecfa7f5727037af4aff480b
cauamhenkel <cauamhenkel@gmail.com>
2026-05-13 18:12:20 -0300

    Update monstro.h

--- TrabalhoFinalAlgProg/monstro.h
+++ TrabalhoFinalAlgProg/monstro.h
@@ -1,3 +1,6 @@
+#ifndef MONSTRO_H
+#define MONSTRO_H
+
 #include "funcoesGerais.h"
 
 typedef struct{
@@ -6,7 +9,12 @@ typedef struct{
 } MONSTRO;
 
 void iniciaMonstros(MONSTRO monstros[10], char mapa[30][30]);
+void regulaMovimentoMonstros(MONSTRO monstros[10], int qtdMonstros, char mapa[30][30]);
 void moveMonstros(MONSTRO monstros[10], int qtdMonstros);
 void desenhaMonstros(MONSTRO monstros[10], int qtdMonstros);
 
 int calculaQtdMonstros(char mapa[30][30]);
+int temChaoNaDireita(MONSTRO monstro, char mapa[30][30]);
+int temChaoNaEsquerda(MONSTRO monstro, char mapa[30][30]);
+
+#endif
