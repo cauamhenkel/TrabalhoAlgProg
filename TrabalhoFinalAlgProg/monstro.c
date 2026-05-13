3fb9a46ad7f6a86d296903bf004ffe0451ae5808
cauamhenkel <cauamhenkel@gmail.com>
2026-05-13 18:12:00 -0300

    Update monstro.c

--- TrabalhoFinalAlgProg/monstro.c
+++ TrabalhoFinalAlgProg/monstro.c
@@ -7,9 +7,29 @@ void iniciaMonstros(MONSTRO monstros[10], char mapa[30][30]){
             if (mapa[i][j]=='E'){
                 monstros[k].posX=j*COMP_LINHA;
                 monstros[k].posY=i*COMP_COLUNA;
+
+                if (GetRandomValue(0,1)==0)
+                    monstros[k].velX=COMP_LINHA/10;
+                else
+                    monstros[k].velX=-COMP_LINHA/10;
+
                 k++;
             }
         }
+    }
+}
+
+void regulaMovimentoMonstros(MONSTRO monstros[10], int qtdMonstros, char mapa[30][30]){
+    for (int i=0 ; i<qtdMonstros ; i++){
+        if (monstros[i].posX-1<0)
+            monstros[i].velX*=-1;
+        else if (monstros[i].velX>0){
+            if (!temChaoNaDireita(monstros[i], mapa))
+                monstros[i].velX*=-1;
+        }
+        else
+            if (!temChaoNaEsquerda(monstros[i], mapa))
+                monstros[i].velX*=-1;
     }
 }
 
@@ -35,4 +55,24 @@ int calculaQtdMonstros(char mapa[30][30]){
         }
     }
     return acc;
+}
+
+int temChaoNaDireita(MONSTRO monstro, char mapa[30][30]){
+    int posXGrid=monstro.posX/COMP_LINHA;
+    int posYGrid=monstro.posY/COMP_COLUNA;
+
+    if (mapa[posYGrid+1][posXGrid+1]=='Z')
+        return 1;
+    else
+        return 0;
+}
+
+int temChaoNaEsquerda(MONSTRO monstro, char mapa[30][30]){
+    int posXGrid=monstro.posX/COMP_LINHA;
+    int posYGrid=monstro.posY/COMP_COLUNA;
+
+    if (mapa[posYGrid+1][posXGrid]=='Z')
+        return 1;
+    else
+        return 0;
 }
