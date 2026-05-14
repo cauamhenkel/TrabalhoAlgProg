4aa3674f6cfdb65278721f883ca7f57ddf561dd1
cauamhenkel <cauamhenkel@gmail.com>
2026-05-14 19:20:28 -0300

    Jogo daora

--- TrabalhoFinalAlgProg/funcoesGerais.h
+++ TrabalhoFinalAlgProg/funcoesGerais.h
@@ -1,3 +1,6 @@
+#ifndef FUNCOES_GERAIS_H
+#define FUNCOES_GERAIS_H
+
 #include "raylib.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,7 +11,16 @@
 #define CABECALHO 50
 #define COMP_LINHA 20
 #define COMP_COLUNA 20
+#define FPS 60
 
-void arqMapa(char nomeMapa[12], const char *nMapa);
+typedef enum {
+    EM_JOGO,
+    PAUSADO,
+    MENU
+} EstadoJogo;
+
+void arqMapa(char nomeMapa[12], int numMapa);
 void criaMapa(char mapa[30][30], char *arq);
 void desenhaMapa(char mapa[30][30]);
+
+#endif
