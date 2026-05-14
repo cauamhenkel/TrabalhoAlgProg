f4e9c31860d67b1b92ca23391c487c14f17ffcd6
cauamhenkel <cauamhenkel@gmail.com>
2026-05-14 19:24:06 -0300

    jogo daora

--- TrabalhoFinalAlgProg/monstro.c
+++ TrabalhoFinalAlgProg/monstro.c
@@ -1,12 +1,14 @@
 #include "monstro.h"
 
-void iniciaMonstros(MONSTRO monstros[10], char mapa[30][30]){
+void iniciaMonstros(MONSTRO monstros[10], char mapa[30][30], int *qtdMonstros){
     int k=0;
     for (int i=0 ; i<30 ; i++){
         for (int j=0 ; j<30 ; j++){
             if (mapa[i][j]=='E'){
                 monstros[k].posX=j*COMP_LINHA;
                 monstros[k].posY=i*COMP_COLUNA;
+
+                (*qtdMonstros)++;
 
                 if (GetRandomValue(0,1)==0)
                     monstros[k].velX=COMP_LINHA/10;
@@ -43,18 +45,6 @@ void desenhaMonstros(MONSTRO monstros[10], int qtdMonstros){
     for (int i=0 ; i<qtdMonstros ; i++){
         DrawRectangle(monstros[i].posX, monstros[i].posY+CABECALHO, COMP_LINHA, COMP_COLUNA, PURPLE);
     }
-}
-
-int calculaQtdMonstros(char mapa[30][30]){
-    int  acc=0;
-    for (int i=0 ; i<30 ; i++){
-        for (int j=0; j<30 ; j++){
-            if (mapa[i][j]=='E'){
-                acc++;
-            }
-        }
-    }
-    return acc;
 }
 
 int temChaoNaDireita(MONSTRO monstro, char mapa[30][30]){
