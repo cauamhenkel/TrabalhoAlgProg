eb49aa34bee0ebb072e665dadaee906821ff253a
cauamhenkel <cauamhenkel@gmail.com>
2026-05-14 19:24:46 -0300

    Jogo daora

--- TrabalhoFinalAlgProg/monstro.h
+++ TrabalhoFinalAlgProg/monstro.h
@@ -8,12 +8,11 @@ typedef struct{
     int posY;
 } MONSTRO;
 
-void iniciaMonstros(MONSTRO monstros[10], char mapa[30][30]);
+void iniciaMonstros(MONSTRO monstros[10], char mapa[30][30], int *qtdMonstros);
 void regulaMovimentoMonstros(MONSTRO monstros[10], int qtdMonstros, char mapa[30][30]);
 void moveMonstros(MONSTRO monstros[10], int qtdMonstros);
 void desenhaMonstros(MONSTRO monstros[10], int qtdMonstros);
 
-int calculaQtdMonstros(char mapa[30][30]);
 int temChaoNaDireita(MONSTRO monstro, char mapa[30][30]);
 int temChaoNaEsquerda(MONSTRO monstro, char mapa[30][30]);
 
