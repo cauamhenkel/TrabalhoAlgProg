162f7c9806724aa648be19f65f47e5416f9d4078
cauamhenkel <cauamhenkel@gmail.com>
2026-05-19 21:29:29 -0300

    Update funcoesGerais.h

--- TrabalhoFinalAlgProg/funcoesGerais.h
+++ TrabalhoFinalAlgProg/funcoesGerais.h
@@ -6,21 +6,40 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define FPS 60
 #define LARGURA 600
 #define ALTURA 600
 #define CABECALHO 50
 #define COMP_LINHA 20
 #define COMP_COLUNA 20
-#define FPS 60
+#define LARG_BOTOES 160
+#define ALT_BOTOES 60
+#define FONTE_BOTOES 20
 
 typedef enum {
     EM_JOGO,
     PAUSADO,
-    MENU
+    MENU,
+    VITORIA,
+    DERROTA
 } EstadoJogo;
 
-void arqMapa(char nomeMapa[12], int numMapa);
-void criaMapa(char mapa[30][30], char *arq);
+typedef enum {
+    MENU_JOGAR,
+    MENU_RANKING,
+    MENU_SAIR
+} EstadoMenu;
+
+typedef enum {
+    PAUSE_CONTINUAR,
+    PAUSE_VOLTAR_AO_MENU,
+    PAUSE_SAIR
+} EstadoPausado;
+
+void reiniciaFase(int *iniciouMapa, int *iniciouPlayer, int *iniciouMonstros);
+void criaMapa(char mapa[30][30], int fase, EstadoJogo *estado);
 void desenhaMapa(char mapa[30][30]);
+void desenhaBotao(int posX, int posY, int largura, int altura, Color corDentro, Color corBorda, const char texto[20]);
+void exibeFase(int fase);
 
 #endif
