d98df44c59fabf9cbc374fe1cc0e51b4babaf3a5
cauamhenkel <cauamhenkel@gmail.com>
2026-05-25 17:38:57 -0300

    Update

--- TrabalhoFinalAlgProg/monstro.h
+++ TrabalhoFinalAlgProg/monstro.h
@@ -4,16 +4,16 @@
 #include "funcoesGerais.h"
 
 typedef struct{
-    int posX; int velX;
+    int posX, velX;
     int posY;
 } MONSTRO;
 
-void iniciaMonstros(MONSTRO monstros[10], char mapa[30][30], int *qtdMonstros);
-void regulaMovimentoMonstros(MONSTRO monstros[10], int qtdMonstros, char mapa[30][30]);
-void moveMonstros(MONSTRO monstros[10], int qtdMonstros);
-void desenhaMonstros(MONSTRO monstros[10], int qtdMonstros);
+void iniciaMonstros(MONSTRO monstros[M_QTD_MAX], char mapa[TILES][TILES], int *qtdMonstros);
+void regulaMovimentoMonstros(MONSTRO monstros[M_QTD_MAX], int qtdMonstros, char mapa[TILES][TILES]);
+void moveMonstros(MONSTRO monstros[M_QTD_MAX], int qtdMonstros);
+void desenhaMonstros(MONSTRO monstros[M_QTD_MAX], int qtdMonstros, Texture2D sprite);
 
-int temChaoNaDireita(MONSTRO monstro, char mapa[30][30]);
-int temChaoNaEsquerda(MONSTRO monstro, char mapa[30][30]);
+int temChaoNaDireita(MONSTRO monstro, char mapa[TILES][TILES]);
+int temChaoNaEsquerda(MONSTRO monstro, char mapa[TILES][TILES]);
 
 #endif
