c6c9b9e85c93022bb86834d9824879e15a3ff0c2
cauamhenkel <cauamhenkel@gmail.com>
2026-05-25 17:35:39 -0300

    Update

--- TrabalhoFinalAlgProg/funcoesGerais.c
+++ TrabalhoFinalAlgProg/funcoesGerais.c
@@ -7,21 +7,21 @@ void reiniciaFase(int *iniciouMapa, int *iniciouPlayer, int *iniciouMonstros){
     (*iniciouMonstros)=0;
 }
 
-void criaMapa(char mapa[30][30], int fase, EstadoJogo *estado){
+void criaMapa(char mapa[TILES][TILES], int fase, EstadoJogo *estado){
 /* Essa função recebe um número de fase e transforma o arquivo .txt formatado dessa fase para ser um mapa em uma matriz 30 por 30 */
 /* Se passar da ultima fase (não tem mais nenhum arquivo para ler) dá a vitória */
     FILE *leitura;                                    // Ponteiro usado para ler o arquivo
-    char nomeArquivo[12];                             // Variável para salvar momentaneamente o nome do arquivo
+    char nomeArquivo[50];                             // Variável para salvar momentaneamente o nome do arquivo
     char ch;                                          // Variável para salvar momentaneamente o caractere lido do arquivo
 
-    sprintf(nomeArquivo, "mapa%d.txt", fase);         // Salva o nome do arquivo que será aberto com base no número da fase de entrada
+    sprintf(nomeArquivo, "assets/levels/mapa%d.txt", fase);         // Salva o nome do arquivo que será aberto com base no número da fase de entrada
 
     if ((leitura = fopen(nomeArquivo, "r")) == NULL){ // Abre o arquivo de entrada no ponteiro e se a leitura der errado (passou todos os mapas)
         (*estado)=VITORIA;                            // Determina vitória (passou da última fase)
     }
     else{
-        for (int i=0 ; i<30 ; i++){
-            for (int j=0 ; j<30 ; j++){
+        for (int i=0 ; i<TILES ; i++){
+            for (int j=0 ; j<TILES ; j++){
                 ch=fgetc(leitura);                    // Lê o caractere do arquivo
                 while (ch=='\n' || ch=='\r')          // Se o caractere lido for algum de formatação (como enter)
                     ch=fgetc(leitura);                // Ignora e pega o próximo caractere, fica em loop até achar um caractere normal
@@ -32,40 +32,12 @@ void criaMapa(char mapa[30][30], int fase, EstadoJogo *estado){
     fclose(leitura);                                  // Fecha o arquivo
 }
 
-void corrigeMapa(char mapa[30][30]){
-    for(int i=0 ; i<30 ; i++){
-        for(int j=0 ; j<30 ; j++){
+void corrigeMapa(char mapa[TILES][TILES]){
+    for(int i=0 ; i<TILES ; i++){
+        for(int j=0 ; j<TILES ; j++){
             if (mapa[i][j]=='D')
                 mapa[i+1][j]='X';
         }
-    }
-}
-
-void desenhaMapa(char mapa[30][30]){
-/* Essa função desenha o mapa usando a matriz como referência */
-    int posX=0, posY=0;              // Variáveis para desenhar os elementos do mapa
-    for (int i=0 ; i<30 ; i++){
-        posX=0;                      // Volta a posição horizontal para o início (começa desenhando da esquerda para a direita)
-        for (int j=0 ; j<30 ; j++){
-            switch(mapa[i][j]){      // Verifica o caractere atual
-                case 'Z':            // Se for Z, desenha o chão
-                    DrawRectangle(posX, posY+CABECALHO, COMP_LINHA, COMP_COLUNA, BROWN);
-                    break;
-                case 'S':            // Se for S, H ou D desenha a escada
-                case 'H':
-                case 'D':
-                    DrawRectangle(posX, posY+CABECALHO, COMP_LINHA, COMP_COLUNA, ORANGE);
-                    break;
-                case 'X':
-                    DrawRectangle(posX, posY+CABECALHO, COMP_LINHA, COMP_COLUNA/2, BROWN);
-                    DrawRectangle(posX, posY+CABECALHO+COMP_COLUNA/2, COMP_LINHA, COMP_COLUNA/2, ORANGE);
-                    break;
-                case 'F':            // Se for F desenha o final da fase
-                    DrawRectangle(posX, posY+CABECALHO, COMP_LINHA, COMP_COLUNA, RED);
-            }
-            posX+=COMP_LINHA;        // Passa para a próxima posição horizontal
-        }
-        posY+=COMP_COLUNA;           // Após uma linha inteira desenhada, passa para a próxima linha
     }
 }
 
@@ -80,5 +52,5 @@ void desenhaBotao(int posX, int posY, int largura, int altura, Color corDentro, 
 
 void exibeFase(int fase){
 /* Essa função exibe no canto da tela a fase atual */
-    DrawText(TextFormat("Fase atual: %d", fase+1), 450, 10, 20, RED);
+    DrawText(TextFormat("Fase atual: %d", fase+1), LARGURA - (MeasureText("Fase atual: 67", FONTE_CABECALHO)), (FONTE_BOTOES / 2), FONTE_CABECALHO, RED);
 }
