039970c77800366c80c5cc3ca67b2ba0599536e4
cauamhenkel <cauamhenkel@gmail.com>
2026-05-27 18:40:13 -0300

    Update
    Added monster state management and ensured active monsters move and draw correctly.

--- TrabalhoFinalAlgProg/monstro.c
+++ TrabalhoFinalAlgProg/monstro.c
@@ -8,6 +8,7 @@ void iniciaMonstros(MONSTRO monstros[M_QTD_MAX], char mapa[TILES][TILES], int *q
             if (mapa[i][j]=='E'){                      // Se encontrar a posição de um monstro no mapa
                 monstros[k].posX=j*COMP_LINHA;         // Define a posição horizontal como o comprimento da linha multiplicado pelo numero da coluna da matriz
                 monstros[k].posY=i*COMP_COLUNA;        // Define a posição vertical como o comprimento da coluna multiplicado pelo numero da linha da matriz
+                monstros[k].estadoMonstro=ATIVO;
 
                 if (GetRandomValue(0,1)==0)
                     monstros[k].velX=M_VEL_X;    // Define a velocidade como positiva
@@ -23,23 +24,27 @@ void iniciaMonstros(MONSTRO monstros[M_QTD_MAX], char mapa[TILES][TILES], int *q
 
 void regulaMovimentoMonstros(MONSTRO monstros[M_QTD_MAX], int qtdMonstros, char mapa[TILES][TILES]){
 /* Essa posição altera a direção do movimento dos monstros checando suas posições no mapa */
-    for (int i=0 ; i<qtdMonstros ; i++){               // Itera um número de vezes igual a quantidade de monstros
-        if (monstros[i].posX-1<0)                      // Verifica se o monstro está tocando a borda esquerda
-            monstros[i].velX*=-1;                      // Se sim, muda sua direção
-        else if (monstros[i].velX>0){                  // Verifica se o monstro está se movendo para a direita
-            if (!temChaoNaDireita(monstros[i], mapa))  // Se sim, verifica se tem chão na sua frente
-                monstros[i].velX*=-1;                  // Se não tiver, muda sua direção
+    for (int i=0 ; i<qtdMonstros ; i++){                   // Itera um número de vezes igual a quantidade de monstros
+        if (monstros[i].estadoMonstro==ATIVO){
+            if (monstros[i].posX-1<0)                      // Verifica se o monstro está tocando a borda esquerda
+                monstros[i].velX*=-1;                      // Se sim, muda sua direção
+            else if (monstros[i].velX>0){                  // Verifica se o monstro está se movendo para a direita
+                if (!temChaoNaDireita(monstros[i], mapa))  // Se sim, verifica se tem chão na sua frente
+                    monstros[i].velX*=-1;                  // Se não tiver, muda sua direção
+            }
+            else                                           // Se não, o monstro está se movendo para a esquerda
+                if (!temChaoNaEsquerda(monstros[i], mapa)) // Verifica se tem chão na sua frente
+                    monstros[i].velX*=-1;                  // Se não tiver, muda sua direção
         }
-        else                                           // Se não, o monstro está se movendo para a esquerda
-            if (!temChaoNaEsquerda(monstros[i], mapa)) // Verifica se tem chão na sua frente
-                monstros[i].velX*=-1;                  // Se não tiver, muda sua direção
     }
 }
 
 void moveMonstros(MONSTRO monstros[M_QTD_MAX], int qtdMonstros){
 /* Essa função muda a posição dos monstros */
-    for (int i=0 ; i<qtdMonstros ; i++){    // Itera um número de vezes igual a quantidade de monstros
-        monstros[i].posX+=monstros[i].velX; // Muda a posição do monstro usando sua velocidade
+    for (int i=0 ; i<qtdMonstros ; i++){        // Itera um número de vezes igual a quantidade de monstros
+        if (monstros[i].estadoMonstro==ATIVO){
+            monstros[i].posX+=monstros[i].velX; // Muda a posição do monstro usando sua velocidade
+        }
     }
 }
 
@@ -48,8 +53,10 @@ void desenhaMonstros(MONSTRO monstros[M_QTD_MAX], int qtdMonstros, Texture2D spr
 Rectangle fonte = {0, 0, sprite.width, sprite.height}; // Retangulo referente ao sprite original
 
     for (int i=0 ; i<qtdMonstros ; i++){    // Itera um número de vezes igual a quantidade de monstros
-        Rectangle destino = {monstros[i].posX, monstros[i].posY + CABECALHO, COMP_LINHA, COMP_COLUNA}; // Retangulo referente ao sprite exibido na tela
-        DrawTexturePro(sprite, fonte, destino, (Vector2){0,0}, 0.0f, PURPLE);
+        if (monstros[i].estadoMonstro==ATIVO){
+            Rectangle destino = {monstros[i].posX, monstros[i].posY + CABECALHO, COMP_LINHA, COMP_COLUNA}; // Retangulo referente ao sprite exibido na tela
+            DrawTexturePro(sprite, fonte, destino, (Vector2){0,0}, 0.0f, PURPLE);
+        }
     }
 }
 
