b54b466b2baa0e3a4bedbe2dc5751045d1ac7842
cauamhenkel <cauamhenkel@gmail.com>
2026-05-27 21:14:09 -0300

    Update

--- TrabalhoFinalAlgProg/graficos.c
+++ TrabalhoFinalAlgProg/graficos.c
@@ -53,3 +53,12 @@ void desenhaMapa(char mapa[TILES][TILES], Texture2D tileset) {
         }
     }
 }
+
+void exibeCabecalho(PLAYER p, int fase, int pontos){
+/* Essa função desenha as informações do cabeçalho */
+    DrawText(TextFormat("Saude: %d", p.saude), COMP_COLUNA/2, (FONTE_BOTOES / 2), FONTE_CABECALHO, RED);
+    DrawText(TextFormat("Tiros: %d", p.qtdTiros), COMP_COLUNA/2, (COMP_COLUNA*1.5)+(FONTE_BOTOES / 2), FONTE_CABECALHO, RED);
+    // Arrumar essa bizarrice acima depois
+    DrawText(TextFormat("Pontos: %d", pontos), (LARGURA/2)-(MeasureText("Pontos : 5000", FONTE_CABECALHO)/2), (FONTE_BOTOES / 2), FONTE_CABECALHO, RED);
+    DrawText(TextFormat("Fase atual: %d", fase+1), LARGURA - (MeasureText("Fase atual: 67", FONTE_CABECALHO)), (FONTE_BOTOES / 2), FONTE_CABECALHO, RED);
+}
