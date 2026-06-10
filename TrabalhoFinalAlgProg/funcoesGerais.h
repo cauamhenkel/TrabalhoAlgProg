b98468c59525305bc5a9b5d136211594d272e9e0
cauamhenkel <cauamhenkel@gmail.com>
2026-06-10 17:01:13 -0300

    Mais comentários

--- TrabalhoFinalAlgProg/funcoesGerais.h
+++ TrabalhoFinalAlgProg/funcoesGerais.h
@@ -6,31 +6,33 @@
 #include <stdlib.h>
 #include <string.h>
 
-/* CONSTANTES GERIAS */
+/* CONSTANTES GERAIS */
 #define FPS 60
 
+/* CONSTANTES PARA O TAMANHO DO GRID E DOS ELEMENTOS DO JOGO */
 #define TILES 30
-#define COMP_LINHA 28
-#define COMP_COLUNA 28
-
-#define LARGURA (COMP_LINHA*TILES) // Define a largura da tela
-#define ALTURA (COMP_COLUNA*TILES) // Define a altura da tela
+#define COMP_LINHA 20
+#define COMP_COLUNA 20
+/* CONSTANTES PARA O TAMANHO DA JANELA */
+#define LARGURA (COMP_LINHA*TILES)
+#define ALTURA (COMP_COLUNA*TILES)
 #define CABECALHO (COMP_COLUNA*2)
-
+/* CONSTANTES PARA OS BOTÕES */
 #define LARG_BOTOES (COMP_LINHA*8)
 #define ALT_BOTOES (COMP_COLUNA*3)
 #define LARG_BOTOES_RANKING LARG_BOTOES
 #define ALT_BOTOES_RANKING ALT_BOTOES*0.75
-
+/* CONSTANTES PARA AS FONTES DO JOGO */
 #define FONTE_GERAL (COMP_LINHA*3)
 #define FONTE_BOTOES (COMP_LINHA)
 #define FONTE_RANKING (COMP_LINHA/2)
 #define FONTE_CABECALHO (FONTE_BOTOES * 1.3)
-
-#define TAM_NOME_RANKING (COMP_LINHA)
+/* CONSTANTES PARA O PLACAR E PONTOS */
+#define TAM_NOME_RANKING 20 // Quantidade de caracteres da string
 #define TAM_PLACAR 10
-#define FRAMES_PARA_REDUZIR_PONTOS FPS*2
+#define QTD_PONTOS_INICIAIS 5000
 #define QTD_REDUZIR_PONTOS 200
+#define FRAMES_PARA_REDUZIR_PONTOS FPS*2
 #define TEMPO_PTS_SUBINDO 1.5f
 
 /* CONSTANTES FISICAS */
@@ -54,6 +56,7 @@
 /* CONSTANTES PROJETIL*/
 #define PROJ_VEL_X COMP_LINHA*0.75
 
+/* Definições de enum para melhor legibilidade */
 typedef enum{
     MENU,
     RANKING,
@@ -90,25 +93,28 @@ typedef struct tipo_placar{
     int pontos;
 } TIPO_PLACAR;
 
+/* Funções para o placar */
 void lePlacar(TIPO_PLACAR placar[TAM_PLACAR]);
 void salvaPlacar(TIPO_PLACAR placar[TAM_PLACAR]);
-
+/* Funções para desenhar os botões */
 void desenhaBotao(int posX, int posY, int largura, int altura, Color corDentro, Color corBorda, const char texto[20]);
 void desenhaBotaoRanking(int posX, int posY, int largura, int altura, Color corDentro, Color corBorda, TIPO_PLACAR placar);
-
+/* Funções para desenhar os diferentes menus do jogo */
 void desenhaTextoMenu(EstadoMenu opcaoMenu);
 void desenhaTextoRanking(TIPO_PLACAR placar[TAM_PLACAR]);
 void desenhaTextoPause(EstadoPausado opcaoPause);
 void desenhaTextoVitoria(int pontos, char nomeTemp[TAM_NOME_RANKING], int *piscando);
 void desenhaTextoDerrota(void);
-
+/* Funções para lidar com o mapa */
 void criaMapa(char mapa[TILES][TILES], int fase);
 void corrigeMapa(char mapa[TILES][TILES]);
+/* Funções para lidar com os pontos e placar */
 void reduzPontos(int *pontos, int *framesReduzirPontos);
 void reiniciaNome(char nomeTemp[TAM_NOME_RANKING]);
 void colocaNoPlacar(TIPO_PLACAR placar[TAM_PLACAR], char nomeTemp[TAM_NOME_RANKING], int pontos);
 void ordenarPlacar(TIPO_PLACAR placar[TAM_PLACAR]);
 
+/* Funções para fazer testes que devolvem booleanos*/
 int vitoria(char mapa[TILES][TILES], int fase);
 
 #endif
