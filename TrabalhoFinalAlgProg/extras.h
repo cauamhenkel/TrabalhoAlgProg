e009336534a54257ec95553497471ff55be4ba1b
cauamhenkel <cauamhenkel@gmail.com>
2026-06-10 17:23:00 -0300

    Comentando um monte de coisa

--- TrabalhoFinalAlgProg/extras.h
+++ TrabalhoFinalAlgProg/extras.h
@@ -1,24 +1,27 @@
 #ifndef EXTRAS_H
 #define EXTRAS_H
 
+#include "raylib.h"
 #include "funcoesGerais.h"
 #include "player.h"
 #include "monstro.h"
-#include "raylib.h"
 #include "graficos.h"
 
+/* Estrutura do projetil */
 typedef struct{
     int posX; float velX;
     int posY;
     EstadoObjeto estado;
 } PROJETIL;
 
+/* Funções relacionadas do projetil */
 void criaProjetil(PLAYER p, PROJETIL *pr);
 void moveProjetil(PROJETIL *pr);
 void processaColisoesProjetil(PROJETIL *pr, char mapa[TILES][TILES]);
 void processaProjetil(PLAYER *p, PROJETIL *pr, char mapa[TILES][TILES], Soundtrack sounds);
-Vector2 mataMonstrosProjetil(PLAYER *p, PROJETIL *pr, MONSTRO monstros[10], int qtdMonstros, int *pontos);
 void desenhaPontosSubindo(Vector2 vec);
 void desenhaProjetil(PROJETIL pr);
+
+Vector2 mataMonstrosProjetil(PLAYER *p, PROJETIL *pr, MONSTRO monstros[10], int qtdMonstros, int *pontos);
 
 #endif
