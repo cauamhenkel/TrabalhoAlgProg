04cd97530045a68abdda264acc087f2136e57754
cauamhenkel <cauamhenkel@gmail.com>
2026-06-10 20:47:58 -0300

    mais comentario

--- TrabalhoFinalAlgProg/monstro.h
+++ TrabalhoFinalAlgProg/monstro.h
@@ -3,20 +3,23 @@
 
 #include "funcoesGerais.h"
 
+/* Estrutura do monstro */
 typedef struct{
     int posX, velX;
     int posY;
+    int frameAtual; float animacaoTimer;
     Direcao dir;
     EstadoObjeto estadoMonstro;
-
-    float animacaoTimer; int frameAtual;
 } MONSTRO;
 
+/* Funções que lidam com o monstro no mapa */
 void iniciaMonstros(MONSTRO monstros[M_QTD_MAX], char mapa[TILES][TILES], int *qtdMonstros);
 void regulaMovimentoMonstros(MONSTRO monstros[M_QTD_MAX], int qtdMonstros, char mapa[TILES][TILES]);
 void moveMonstros(MONSTRO monstros[M_QTD_MAX], int qtdMonstros);
 
+/* Funções booleanas de suporte */
 int temChaoNaDireita(MONSTRO monstro, char mapa[TILES][TILES]);
 int temChaoNaEsquerda(MONSTRO monstro, char mapa[TILES][TILES]);
 
 #endif
+
